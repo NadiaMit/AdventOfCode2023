274de1150be5bcf46be86e5f2c433bd570dfd3c7
reputation <100577703+NadiaMit@users.noreply.github.com>
2023-11-26 17:39:13 +0100

    fixed template and helpers

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,10 +1,10 @@
 
 # get current day number
-def getCurrentDay(file_path):
-    return file_path.split('\\')[-1].split('.')[0].replace('day', '')
+def get_current_day(file_path):
+    return file_path.split('\\')[-1].split(".")[0].replace("day", "")
 
 # reads input from the current days txt file
-def readInput(day, test=False):
-    path = f'./inputs/day{day}.txt' if not test else f'./inputs/test.txt'
+def read_input(day, test=False):
+    path = f"./inputs/day{day}.txt" if not test else f"./inputs/test.txt"
     with open(path, 'r') as file:
-        return file.read().splitlines()
+        return file.read()
--- helpers/template.py
+++ helpers/template.py
@@ -1,20 +1,21 @@
 import sys
-sys.path.insert(1, sys.path[0].replace('days', 'helpers'))
+sys.path.insert(1, sys.path[0].replace("days", "helpers"))
 import helpers as helpers
 
 # get daily input
-day = helpers.getCurrentDay(__file__)
-input = helpers.readInput(day, test=True)
+day = helpers.get_current_day(__file__)
+input = helpers.read_input(day, test=True)
+
+# code for both parts
+
 
 # part 1
-def part1():
-    return 0
+result_part_1 = 0
 
 # part 2
-def part2():
-    return 0
+result_part_2 =  0
 
 # run both parts and print the results
-print(f'--- Day {day}: ---')
-print(f'Part 1: {part1()}')
-print(f'Part 2: {part2()}')
+print(f"--- Day {day}: ---")
+print(f"Part 1: {result_part_1}")
+print(f"Part 2: {result_part_2}")

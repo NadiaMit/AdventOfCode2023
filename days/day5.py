32182d691f11e43f698cfe03cef33478fb8dd16d
reputation <100577703+NadiaMit@users.noreply.github.com>
2023-12-05 11:22:51 +0100

    improved day 5 part 1 even more

--- days/day5.py
+++ days/day5.py
@@ -7,6 +7,20 @@ day = helpers.get_current_day(__file__)
 input = helpers.read_input(day, test=False)
 
 # code for both parts
+def calculate_seed_locations(instruction_list, seeds):
+    # loop through mapping of every step
+    for instruction in instruction_list:
+        # parse mapping steps
+        mapping_list = instruction.split(':\n')[1]
+        mapping = []
+        for line in mapping_list.split('\n'):
+            mapping.append([int(num) for num in line.split()])
+        
+        # calculate the new seed values for every mapping step
+        seeds = [get_mapping(mapping, seed) for seed in seeds]
+    
+    return seeds
+
 def get_mapping(mapping_list, seed):
     return_value = 0    
     # for every mapping in the mapping list
@@ -20,28 +34,16 @@ def get_mapping(mapping_list, seed):
     
     return return_value if return_value != 0 else seed
 
-seeds = []
+
 instructions = input.split('\n\n')
-
-# get seeds from input
-seeds = [int(seed) for seed in instructions[0].split(':')[1].split()]
-
-# loop through mapping of every step
-for i in range(1, len(instructions)):
-    # parse mapping steps
-    mapping_list = instructions[i].split(':\n')[1]
-    mapping = []
-    for line in mapping_list.split('\n'):
-        mapping.append([int(num) for num in line.split()])
-    
-    # calculate the new seed values for every mapping step
-    seeds = [get_mapping(mapping, seed) for seed in seeds]
+# parse seeds into set
+seeds = {int(seed) for seed in instructions[0].split(':')[1].split()}
 
 # part 1
-result_part_1 = min(seeds)
+result_part_1 = min(calculate_seed_locations(instructions[1:], seeds))
 
 # part 2
-result_part_2 =  0
+result_part_2 = 0
 
 # print the results
 print(f"--- Day {day}: ---")

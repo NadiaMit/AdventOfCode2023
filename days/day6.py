8aede22fa1ae5479047771eb69a8689115d3aed1
reputation <100577703+NadiaMit@users.noreply.github.com>
2023-12-06 11:08:22 +0100

    improved day 6

--- days/day6.py
+++ days/day6.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(1, sys.path[0].replace("days", "helpers"))
 import helpers as helpers
+import math
 
 # get daily input
 day = helpers.get_current_day(__file__)
@@ -12,18 +13,20 @@ times = input[0].strip().split(':')[1].split()
 distances = input[1].strip().split(':')[1].split()
 
 # calculate the number of ways to win
-def calculate_num_ways(time, distance, num_ways):
-    for i in range(1, time):
-        num_ways += (i*(time-i) > distance)
-    return num_ways
+def calculate_num_ways(time, distance):
+    # use quadratic formula
+    sqare_root = math.sqrt(time**2 - (4 * distance))
+    min = int(((-time) + sqare_root)/-2)
+    max = int(((-time) - sqare_root)/-2)
+    
+    return (max - min)
 
 # part 1
 result_part_1 = 1
 
 # for every race calculate the number of ways to win and multiply them
 for i in range(len(times)):
-    result_part_1 *=  calculate_num_ways(int(times[i]), int(distances[i]), 0)
-
+    result_part_1 *=  calculate_num_ways(int(times[i]), int(distances[i]))
 
 # part 2
 # repartse time and distance of race
@@ -31,7 +34,7 @@ time = int(''.join(times))
 distance = int(''.join(distances))
 
 # calculate number of ways to win for the race
-result_part_2 = calculate_num_ways(time, distance, 0)
+result_part_2 = calculate_num_ways(time, distance)
 
 # print the results
 print(f"--- Day {day}: ---")

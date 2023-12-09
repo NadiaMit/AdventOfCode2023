698dfa7e07715d615bc2d9aa74e6a58296cd2f3d
reputation <100577703+NadiaMit@users.noreply.github.com>
2023-12-09 16:26:04 +0100

    finished day 9

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,5 @@
-![](https://img.shields.io/badge/stars%20⭐-16-yellow)
-![](https://img.shields.io/badge/days%20completed%20📅-8-blue)
+![](https://img.shields.io/badge/stars%20⭐-18-yellow)
+![](https://img.shields.io/badge/days%20completed%20📅-9-blue)
 
 # Advent of Code 2023 in Python [![Python](https://skillicons.dev/icons?i=python)](https://skillicons.dev)
 
---
+++ days/day9.py
@@ -0,0 +1,61 @@
+import sys
+sys.path.insert(1, sys.path[0].replace("days", "helpers"))
+import helpers as helpers
+
+# get daily input
+day = helpers.get_current_day(__file__)
+input = helpers.read_input(day, test=False)
+
+# code for both parts
+# subtraction function for part 2
+def sub(list):
+    value = list[-2] - list[-1]
+    reverse_list = reversed(list[0:-2])
+    
+    for num in reverse_list:
+        value = num - value
+    
+    return value
+
+# inizialize both results
+result_part_1 = 0
+result_part_2 =  0
+
+# for every line in input
+for line in input:
+    # parse line as int array
+    numbers = [int(num) for num in line.split()]
+    
+    # save the first and last numbers for before and after history
+    first_nums = [numbers[0]]
+    last_nums = [numbers[-1]]
+    
+    # calculate difference of numbers until all values are zero
+    all_zeros = False
+    while not all_zeros:
+        # calculate differences of current numbers
+        diff = []
+        for i in range(0, len(numbers)-1):
+            diff.append(numbers[i+1] - numbers[i])
+        
+        # set numbers to be the differences
+        numbers = diff
+        
+        # save first and last number seperately for before and after history
+        first_nums.append(numbers[0])
+        last_nums.append(numbers[-1])
+        
+        # check if all numbers are zero
+        if numbers.count(0) == len(numbers):
+            all_zeros = True
+    
+    # part 1
+    result_part_1 += sum(last_nums)
+    
+    # part 2
+    result_part_2 += sub(first_nums)
+
+# print the results
+print(f"--- Day {day}: ---")
+print(f"Part 1: {result_part_1}") # 1666172641
+print(f"Part 2: {result_part_2}") # 933
